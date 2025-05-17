56e29f43b674d2a23bc2c465a52bf59a8b97a066
JMJTC <2801987135@qq.com>
2025-05-18 01:03:52 +0800

    init

---
+++ src/main/java/com/example/xiaomicar/XiaomicarApplication.java
@@ -0,0 +1,13 @@
+package com.example.xiaomicar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiaomicarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiaomicarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xiaomicar/XiaomicarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xiaomicar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaomicarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
