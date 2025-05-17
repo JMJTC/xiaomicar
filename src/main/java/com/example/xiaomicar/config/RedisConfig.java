87525e2ebd8a46c5ae4ab4570096b199ec960c8e
JMJTC <2801987135@qq.com>
2025-05-18 01:21:25 +0800

    redis swagger 配置类

---
+++ src/main/java/com/example/xiaomicar/config/RedisConfig.java
@@ -0,0 +1,36 @@
+package com.example.xiaomicar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @Author: jmjtc
+ * @CreateTime: 2025-05-14
+ * @Description:
+ * @Version: 1.0
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        
+        // 设置key的序列化方式
+        template.setKeySerializer(new StringRedisSerializer());
+        // 设置value的序列化方式
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        // 设置hash key的序列化方式
+        template.setHashKeySerializer(new StringRedisSerializer());
+        // 设置hash value的序列化方式
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        
+        template.afterPropertiesSet();
+        return template;
+    }
+}
---
+++ src/main/java/com/example/xiaomicar/config/SwaggerConfig.java
@@ -0,0 +1,39 @@
+package com.example.xiaomicar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author jmjtc
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.xiaomicar.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("火车票座位管理系统接口文档")
+                .description("使用Swagger2构建RESTful APIs")
+                .contact(new Contact("jmj", "", ""))
+                .version("1.0")
+                .build();
+    }
+} 
